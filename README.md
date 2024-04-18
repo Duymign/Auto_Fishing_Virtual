e8ec341eb596257471d2b4cec9637fe8fdebfab9
Duymign1105 <147126083+Duymign@users.noreply.github.com>
2024-04-18 16:21:36 +0700

    commit
    First

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Auto_Fishing_Virtual
- 
+# README File
+
+---
+
+## Project Name: Victor Fishing
+
+## Version: 1.0
+
+## Author: Victor
+
+---
+
+### Description:
+
+WHY CODE: PUBLISH and EVERY BODY CAN CHECK AND IMPROVE THAT
+FOR YOU <3
+
+---
+
+### Installation:
+
+1. **Download main.py file:**
+    - Click on the "Download" button or clone the repository to get the main.py file.
+
+2. **Ensure Screen Resolution:**
+    - Make sure your screen resolution is set to 1920x1080 with a scaling ratio of 100%.
+
+3. **Install Python 3.10:**
+    - Download and install Python 3.10 from the official website: [Python.org](https://www.python.org/downloads/).
+
+    -If you use window, you can find it on microsoft store ez
+
+4. **Install Required Libraries:**
+    - Open your command prompt or terminal and run the following commands:
+        ```
+        pip install numpy
+        pip install pillow
+        pip install opencv-python
+        pip install pyautogui
+        ```
+
+5. **Navigate to File Location and Run:**
+    - Open your command prompt or terminal, navigate to the directory where the main.py file is located, and run the following command:
+        ```
+        python main.py
+        ```
+
+---
+
+### Hope it can help all you guys, for save your time
+
+
---
+++ main2.py
@@ -0,0 +1,92 @@
+import numpy as np
+from PIL import ImageGrab
+import time
+import os
+from datetime import datetime
+import cv2
+import pyautogui
+import random
+
+x1, y1, x2, y2 = 658, 686, 1152, 737
+
+save_folder = "images"
+
+if not os.path.exists(save_folder):
+    os.makedirs(save_folder)
+
+# Hàm phát hiện màu
+def detect_color():
+    # COLOR FOR DETECT ALL ISLAND 
+    target_colors = [
+        
+        (22, 42, 219),  # Chuyển sang RGB: (219, 42, 22)
+        (19, 45, 221),  # Chuyển sang RGB: (221, 45, 19)
+        (23, 45, 221),  # Chuyển sang RGB: (221, 45, 23)
+        (15, 38, 222),  # Chuyển sang RGB: (222, 38, 15)
+        (15, 38, 222),  # Chuyển sang RGB: (222, 38, 15)
+        (12, 48, 212),  # Chuyển sang RGB: (212, 48, 12)
+        (23, 44, 216)   # Chuyển sang RGB: (216, 44, 23)
+    ]
+    
+   
+    screenshot = np.array(ImageGrab.grab(bbox=(x1, y1, x2, y2)))
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d-%H%M%S-%f")[:-3]
+    image_path = os.path.join(save_folder, f"{timestamp}.png")
+   
+    bgr_image = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
+    
+    for target_color in target_colors:
+        
+        lower_color = np.array(target_color, dtype="uint8")
+        upper_color = np.array(target_color, dtype="uint8")
+
+        mask = cv2.inRange(bgr_image, lower_color, upper_color)
+
+        count = cv2.countNonZero(mask)
+
+        if count > 0:
+           
+            with open("./log.txt", "a") as log_file:
+                log_file.write("Detected\n")
+            print("Detected")
+            return True
+    
+    return False
+
+
+def perform_action():
+    # Tạo ra một số thời gian ngẫu nhiên cho mỗi hành động
+    click_delay = random.uniform(0.08, 0.15)
+    action_delay = random.uniform(0, 0.15)
+    retry_delay = random.uniform(7, 9)
+
+    # Di chuyển chuột đến vị trí cụ thể và thực hiện click
+    x = random.uniform(1200, 1220)
+    y = random.uniform(689, 705)
+    pyautogui.click(x, y, duration=click_delay)
+    time.sleep(action_delay)
+    action_delay = random.uniform(0.3, 0.5)
+    x = random.uniform(950, 980)
+    y = random.uniform(689, 705)
+    click_delay = random.uniform(0.7, 2.5)
+    pyautogui.click(x, y, duration=click_delay)
+    time.sleep(retry_delay)
+
+counter = 0
+
+while True:
+    ok = detect_color()
+    
+    if ok:
+        perform_action()
+        counter = 0
+
+    else:
+        if(counter > 100):
+            
+            pyautogui.click(x=1220, y=1020)
+            counter = 0
+            
+        else:
+            counter += 1
